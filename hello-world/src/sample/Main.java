7976e35e205d7844aeeb566012eae8d0ee75f380
anushiyat <anushiya.16@cse.mrt.ac.lk>
2019-11-23 16:13:43 +0530

    formatting label using fxml

--- hello-world/src/sample/Main.java
+++ hello-world/src/sample/Main.java
@@ -2,17 +2,37 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+//import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//
+//        Label greeting = new Label("Welcome to JavaFX");
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+//        greeting.setTextFill(Color.BLUE);
+//        root.getChildren().add(greeting);
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 275));
         primaryStage.show();
     }
 
