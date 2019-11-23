3b9af3a9efb0bea042c6d46a3c0272e0791cc6b1
anushiyat <anushiya.16@cse.mrt.ac.lk>
2019-11-23 23:54:49 +0530

    adding basic events

--- events/src/sample/Controller.java
+++ events/src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+//import java.awt.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField name;
+    public void onOkayClicked(){
+        System.out.println("Okay, your name is "+ name.getText());
+    }
+
+    public void onCancelClicked(){
+        System.out.println("Cancel");
+    }
 }
