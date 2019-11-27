05102cb6f38b6f843fdc89b9a30b2b3f417d06e7
anushiyat <anushiya.16@cse.mrt.ac.lk>
2019-11-27 19:27:34 +0530

    action events

--- events/src/sample/Controller.java
+++ events/src/sample/Controller.java
@@ -3,15 +3,28 @@ package sample;
 //import java.awt.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
 
 public class Controller {
     @FXML
-    private TextField name;
+    private TextField nameField;
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private Button helpButton;
+
+    @FXML
     public void onOkayClicked(){
-        System.out.println("Okay, your name is "+ name.getText());
+        System.out.println("Okay, your name is "+ nameField.getText());
     }
 
-    public void onCancelClicked(){
-        System.out.println("Cancel");
+    @FXML
+    public void onClicked(ActionEvent e){
+        if(e.getSource().equals(cancelButton)){
+            System.out.println("Cancel");
+        }else if(e.getSource().equals(helpButton)){
+            System.out.println("Help");
+        }
     }
 }
